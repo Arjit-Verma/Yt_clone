b5ef5b0ea950aa8b76124356a47a6d3d170ef5fd
Arjit <arjitverma73@gmail.com>
2024-05-24 00:53:36 +0530

    Added all the elements in the hidden sidebar and made the button component as export

--- src/layouts/SideBar.tsx
+++ src/layouts/SideBar.tsx
@@ -1,3 +1,43 @@
+import { Clapperboard, Home, Library, Repeat } from "lucide-react";
+import { ElementType } from "react";
+import { buttonStyles } from "../components/button";
+import { twMerge } from "tailwind-merge";
+
 export function SideBar() {
-  return null;
+  return (
+    <>
+      <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-2 lg:hidden ">
+        <SmallSidebarItem Icon={Home} title="Home" url="/" />
+        <SmallSidebarItem Icon={Repeat} title="React" url="/shorts" />
+        <SmallSidebarItem
+          Icon={Clapperboard}
+          title="Subscription"
+          url="/subscription"
+        />
+        <SmallSidebarItem Icon={Library} title="Library" url="/library" />
+      </aside>
+      <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 flex"></aside>
+    </>
+  );
+}
+type SmallSidebarItemProps = {
+  Icon: ElementType;
+  title: string;
+  url: string;
+};
+function SmallSidebarItem({ Icon, title, url }: SmallSidebarItemProps) {
+  return (
+    <a
+      href={url}
+      className={twMerge(
+        buttonStyles({
+          variant: "ghost",
+        }),
+        " py-4 px-1 flex flex-col itmes-center rounded-lg gap-1"
+      )}
+    >
+      <Icon className="w-5 h-6 ml-2"></Icon>
+      <div className="text-sm ">{title}</div>
+    </a>
+  );
 }
